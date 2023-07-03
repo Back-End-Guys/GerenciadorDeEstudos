ba1c82eb2da702215fcc977e0cb07b67c8977cf4
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-03 16:38:28 -0300

    Criando Classes dos repositórios

---
+++ ListaExerciciosMariana.Infra/ModuloMateria/RepositorioMateriaEmSql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExerciciosMariana.Infra.ModuloMateria
+{
+    internal class RepositorioMateriaEmSql
+    {
+    }
+}
