82470c4a6b5ba48c6aea56857dd6c9041c84e30b
apaolaoliveira <paolaoliveira.dev@gmail.com>
2023-07-03 16:45:05 -0300

    public classes

--- ListaExerciciosMariana.Infra/ModuloDisciplina/RepositorioDisciplinaEmSql.cs
+++ ListaExerciciosMariana.Infra/ModuloDisciplina/RepositorioDisciplinaEmSql.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ListaExerciciosMariana.Infra.ModuloDisciplina
+﻿namespace ListaExerciciosMariana.Infra.ModuloDisciplina
 {
-    internal class RepositorioDisciplinaEmSql
+    public class RepositorioDisciplinaEmSql
     {
     }
 }
--- ListaExerciciosMariana.Infra/ModuloMateria/RepositorioMateriaEmSql.cs
+++ ListaExerciciosMariana.Infra/ModuloMateria/RepositorioMateriaEmSql.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ListaExerciciosMariana.Infra.ModuloMateria
+﻿namespace ListaExerciciosMariana.Infra.ModuloMateria
 {
-    internal class RepositorioMateriaEmSql
+    public class RepositorioMateriaEmSql
     {
     }
 }
