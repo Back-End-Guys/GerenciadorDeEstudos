35b236b9bec0ce443be9756260d274d32f85a98d
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-03 18:02:28 -0300

    Implementando Controlador de Questão

--- ListaExerciciosMariana/ModuloQuestao/ControladorQuestao.cs
+++ ListaExerciciosMariana/ModuloQuestao/ControladorQuestao.cs
@@ -1,7 +1,12 @@
-﻿namespace ListaExerciciosMariana.WinForm.ModuloQuestao
+﻿using ListaExerciciosMariana.Dominio.ModuloQuestao;
+
+namespace ListaExerciciosMariana.WinForm.ModuloQuestao
 {
     public class ControladorQuestao : ControladorBase
     {
+        TabelaQuestaoControl tabelaQuestao;
+        private IRepositorioQuestao repositorioQuestao;
+
         public override string ToolTipInserir => "Inserir nova questão";
 
         public override string ToolTipEditar => "Editar questão existente";
@@ -10,27 +15,100 @@
 
         public override void Inserir()
         {
-            throw new NotImplementedException();
+            TelaQuestaoForm telaQuestao = new TelaQuestaoForm();
+
+            DialogResult opcaoEscolhida = telaQuestao.ShowDialog();
+
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                Questao questao = telaQuestao.ObterQuestao();
+
+                repositorioQuestao.Inserir(questao);
+            }
+            CarregarQuestoes();
         }
+
 
         public override void Editar()
         {
-            throw new NotImplementedException();
+            Questoes questaoSelecionada = ObterQuestaoSelecionada();
+
+            if (questaoSelecionada == null)
+            {
+                MessageBox.Show("Selecione uma questão primeiro", "Edição de Questão", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                return;
+            }
+
+            TelaQuestoesForm telaQuestao = new TelaQuestoesForm();
+            telaQuestao.Text = "Editar questão existente";
+
+            telaQuestao.ConfigurarTela(questaoSelecionada);
+
+            DialogResult opcaoEscolhida = telaQuestao.ShowDialog();
+
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                Questoes questao = telaQuestao.ObterQuestao();
+
+                repositorioQuestoes.Editar(questao.id, questao);
+            }
+
+            CarregarQuestoes();
         }
 
         public override void Excluir()
         {
-            throw new NotImplementedException();
+            Questoes questao = ObterQuestaoSelecionada();
+
+            if (questao == null)
+            {
+                MessageBox.Show($"Selecione uma questao primeiro!",
+                    "Exclusão de Questões",
+                    MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            DialogResult opcaoEscolhida = MessageBox.Show($"Deseja excluir a questão?", "Exclusão de Questões",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                repositorioQuestoes.Excluir(questao);
+            }
+
+            CarregarQuestoes();
+        }
+
+        private Questoes ObterQuestaoSelecionada()
+        {
+            int id = tabelaQuestao.ObterIdSelecionado();
+
+            return repositorioQuestoes.SelecionarPorId(id);
+        }
+
+
+        private void CarregarQuestoes()
+        {
+            List<Questoes> listaQuestoes = repositorioQuestoes.SelecionarTodos();
+
+            tabelaQuestao.AtualizarRegistros(listaQuestoes);
         }
 
         public override UserControl ObterListagem()
         {
-            throw new NotImplementedException();
+            if (tabelaQuestao == null)
+                tabelaQuestao = new TabelaQuestoesControl();
+
+            CarregarQuestoes();
+
+            return tabelaQuestao;
         }
 
         public override string ObterTipoCadastro()
         {
-            return "Cadastro de questão";
+            return "Registro de Questão";
         }
     }
 }
