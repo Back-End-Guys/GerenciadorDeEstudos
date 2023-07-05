cf4f7f5699b4c8f5bc71f28d0a7c7c459fb3fbbc
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-05 16:07:03 -0300

    Atualizando Banco

---
+++ GeradorDeTestes.Infra.Dados.Sql.ProjetoDatabase/dbo/Tables/TbAlternativa.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TbAlternativa] (
+    [id]                  INT          IDENTITY (1, 1) NOT NULL,
+    [AlternativaResposta] VARCHAR (50) NOT NULL,
+    [Verdadeiro]          BIT          NOT NULL,
+    [Questão_Id]          INT          NOT NULL,
+    CONSTRAINT [PK_TbAlternativa] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_TbAlternativa_Questao] FOREIGN KEY ([Questão_Id]) REFERENCES [dbo].[Questao] ([id])
+);
+
