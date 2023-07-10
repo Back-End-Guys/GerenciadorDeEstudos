e8b1a93c1bda857bed3debbfdfd5c80b9d967554
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-10 19:44:16 -0300

    Atualizando Banco

--- GeradorDeTestes.Infra.Dados.Sql.ProjetoDatabase/dbo/Tables/TbTeste.sql
+++ GeradorDeTestes.Infra.Dados.Sql.ProjetoDatabase/dbo/Tables/TbTeste.sql
@@ -2,13 +2,15 @@
     [Id]                 INT           IDENTITY (1, 1) NOT NULL,
     [Titulo]             VARCHAR (MAX) NOT NULL,
     [Disciplina_Id]      INT           NOT NULL,
-    [Materia_Id]         INT           NOT NULL,
+    [Materia_Id]         INT           NULL,
     [QuantidadeQuestoes] INT           NOT NULL,
     [ProvaRecuperacao]   BIT           NOT NULL,
     CONSTRAINT [PK_TbTeste] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_TbTeste_Disciplina] FOREIGN KEY ([Disciplina_Id]) REFERENCES [dbo].[Disciplina] ([Id]),
     CONSTRAINT [FK_TbTeste_TbMateria] FOREIGN KEY ([Materia_Id]) REFERENCES [dbo].[TbMateria] ([Id])
 );
+
+
 
 
 
